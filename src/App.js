30ffe3cd5e30ffb977163686fc757a43e7e14518
Kaio Oliveira <kaiohenrique2601@gmail.com>
2020-09-28 10:53:51 -0300

    feat:new component default-input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import { DefaultInput } from "./components/default-input";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><DefaultInput/></div>;
 }
 
 export default App;
---
+++ src/components/default-input/index.tsx
@@ -0,0 +1,7 @@
+import { Input } from "antd";
+import styled from "styled-components";
+
+export const DefaultInput = styled(Input)`
+  max-width: 80%;
+  width:100%;
+`;

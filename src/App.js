32bdc4987cf7674e5bff4342fd430e44a756fd81
Cristine <cristine.balsini@gmail.com>
2020-09-28 23:08:28 -0300

    wip: responsive header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Routes from "./routes/routes.js";
+import Header from "./components/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes />
     </div>
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+const useWindowSize = () => {
+  const initialState = {
+    width: 0,
+    height: 0,
+  };
+
+  const [windowSize, setWindowSize] = useState(initialState);
+
+  useEffect(() => {
+    const handleSize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleSize);
+    handleSize();
+    return () => window.removeEventListener("resize", handleSize);
+  }, []);
+  return windowSize;
+};
+
 const Header = () => {
-  return <div>teste</div>;
+  const width = useWindowSize().width;
+  console.log(width, "my pretty windowSize");
+
+  return (
+    <div>
+      {(width > 768 && (
+        <MaxHeader>
+          <div>testeHEADER</div>
+          <div>teste2HEADER</div>
+          <div>teste3HEADER</div>
+        </MaxHeader>
+      )) || <MinHeader></MinHeader>}
+    </div>
+  );
 };
 
 export default Header;
+
+const MaxHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 97px;
+  color: #ffffff;
+  font-size: 24px;
+  background-color: #365083;
+
+  div {
+    margin-left: 50px;
+  }
+`;
+
+const MinHeader = styled.div`
+  width: 100%;
+  height: 97px;
+  background-color: #365083;
+`;

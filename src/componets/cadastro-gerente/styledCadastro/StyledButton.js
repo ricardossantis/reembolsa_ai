617f9fc484b02a7648ddb52fb7045b8db481fcf6
Franciele <Franciele Cristine>
2020-10-05 18:41:21 -0300

    fixed: buttons position.

--- src/componets/cadastro-gerente/styledCadastro/StyledButton.js
+++ src/componets/cadastro-gerente/styledCadastro/StyledButton.js
@@ -10,10 +10,14 @@ text-decoration: none;
 
 const StyledButtonGreen  = styled(Button) `
 color: green;
+display: flex;
+align-self: center;
+margin-top: 10%;
 font-size: 2rem;
 text-decoration: none;
 background-color: transparent;
 border: none;
+box-shadow: none;
 &:hover{
     background-color: transparent;
     }
--- src/componets/cadastro-gerente/styledCadastro/container-buttons.js
+++ src/componets/cadastro-gerente/styledCadastro/container-buttons.js
@@ -4,6 +4,7 @@ const ContainerButtons = styled.div `
     display:flex;
     justify-content: space-around;
     flex-direction: row;
+    height: 50vh;
 @media(max-width: 670px ){
     justify-content: space-between;
     flex-direction: row;

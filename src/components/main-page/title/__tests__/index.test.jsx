8189c2e3bbf708445d40da6ba65994f6d85243fe
Franciele <Franciele Cristine>
2020-10-06 14:36:25 -0300

    fixed: change test name

--- src/components/main-page/title/__tests__/index.test.jsx
+++ src/components/main-page/title/__tests__/index.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Title from '../index';
 import renderer from 'react-test-renderer';
 
-it("renderer", () =>{
+it("Title", () =>{
     const tree = renderer
     .create(<Title bckTitle="#365083" colorTitle="#FFFFFF">{'Test'}</Title>)
     .toJSON();

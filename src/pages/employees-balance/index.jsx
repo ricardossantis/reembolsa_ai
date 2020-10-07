aa3512beca5ee8cd624f7df92a9c0dbc92bcfae1
Cristine <cristine.balsini@gmail.com>
2020-10-07 16:54:38 -0300

    wip: refound

--- src/pages/employees-balance/index.jsx
+++ src/pages/employees-balance/index.jsx
@@ -15,12 +15,8 @@ import {
 const Saldo = () => {
   const stateAuth = useSelector((state) => state.authentication);
   const id = stateAuth.user.id - 1;
-
   const width = useWindowSize().width;
-
   const token = stateAuth.auth;
-  const [resul, setResul] = useState();
-  const [value, setValue] = useState([]);
 
   const [circleColor, setCircleColor] = useState("");
   const colors = {
@@ -28,17 +24,13 @@ const Saldo = () => {
     yellow: "#F9BB1D",
     green: "#2CD3B5",
   };
+  const amountLimit = useSelector(
+    (state) => state.authentication.user.amountLimit
+  );
 
-  const showLimit = () => {
-    if (amountLimit <= 100) {
-      setCircleColor(colors.red);
-    } else if (amountLimit <= 300) {
-      setCircleColor(colors.yellow);
-    } else {
-      setCircleColor(colors.green);
-    }
-  };
-  const refundRequest = () => {
+  const [renderResult, setRenderResult] = useState("");
+
+  useEffect(() => {
     api
       .get("/refunds", {
         headers: {
@@ -46,40 +38,40 @@ const Saldo = () => {
         },
       })
       .then((res) => {
-        res.data
+        const value = res.data
           .filter((item) => item.userId === id && item.status === "approved")
-          .map((item) => ({ refoundApproved: parseInt(item.value) }));
+          .map((item) => ({ refoundApproved: parseInt(item.value) }))
+          .reduce((x, y) => x + y.refoundApproved, 0);
+        setRenderResult(amountLimit - value);
       })
       .catch((err) => console.log(err));
+  }, []);
+
+  const showLimit = () => {
+    if (renderResult <= 100) {
+      setCircleColor(colors.red);
+    } else if (renderResult <= 300) {
+      setCircleColor(colors.yellow);
+    } else {
+      setCircleColor(colors.green);
+    }
   };
-
-  useEffect(() => console.log(refundRequest), []);
-//testar
-  setResul(value);
-  console.log(resul);
-
-  const amountLimit = useSelector(
-    (state) => state.authentication.user.amountLimit
-  );
-
-  // console.log(listValues, "lista aqui")
-
-  useEffect(showLimit, []);
-
+  useEffect(() => showLimit(), []);
+ 
   return (
     <>
       {(width > 768 && (
         <Box>
           <Title>Saldo disponível</Title>
           <BoxCircle>
-            <Circle color={circleColor}>R$ {amountLimit}</Circle>
+            <Circle color={circleColor}>R$ {renderResult}</Circle>
           </BoxCircle>
         </Box>
       )) || (
         <Box>
           <MinTitle>Saldo disponível</MinTitle>
           <BoxCircle>
-            <MinCircle color={circleColor}>R$ {amountLimit}</MinCircle>
+            <MinCircle color={circleColor}>R$ {renderResult}</MinCircle>
           </BoxCircle>
         </Box>
       )}

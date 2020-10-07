51213ef1bbdff71f754ef038fb8a7a5bd7ffb26a
Kaio Oliveira <kaiohenrique2601@gmail.com>
2020-10-07 09:57:17 -0300

    fix package-lock.json

--- src/pages/login-page/_index_.test.jsx
+++ src/pages/login-page/_index_.test.jsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import TestRenderer from "react-test-renderer";
 import LoginPage from "./index";
 
-const setStateSpy = jest.fn();
+import "antd/dist/antd.css";
+
 jest.mock("react-redux", () => ({
   useDispatch: () => jest.fn(),
-  useSelector: ()=>{return {}},
+  useSelector: () => 1,
 }));
+
+jest.mock("react-router-dom", () => ({
+  Link: { $$typeof: "Symbol(react.forward_ref)", displayName: "Link" },
+  useHistory: { push: jest.fn() },
+}));
+
 test("Post authentication", () => {
   const tree = TestRenderer.create(<LoginPage />).toJSON();
   expected(tree).toMatchSnapshot();

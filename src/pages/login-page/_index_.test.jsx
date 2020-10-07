9cc227c34dcf954a5649efd2d28b933ab855a3c7
Kaio Oliveira <kaiohenrique2601@gmail.com>
2020-10-07 11:20:10 -0300

    feat:test snapshot login page

--- src/pages/login-page/_index_.test.jsx
+++ src/pages/login-page/_index_.test.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import TestRenderer from "react-test-renderer";
 import LoginPage from "./index";
 
-import "antd/dist/antd.css";
-
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  })) 
 jest.mock("react-redux", () => ({
   useDispatch: () => jest.fn(),
-  useSelector: () => 1,
+  useSelector: () => ({user:{accessLevel:undefined}}),
 }));
 
 jest.mock("react-router-dom", () => ({
-  Link: { $$typeof: "Symbol(react.forward_ref)", displayName: "Link" },
-  useHistory: { push: jest.fn() },
+Link: ({children})=> <a>{children}</a>,
+  useHistory:jest.fn(),
 }));
 
 test("Post authentication", () => {
   const tree = TestRenderer.create(<LoginPage />).toJSON();
-  expected(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });

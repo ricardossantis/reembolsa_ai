dbdcb3dc62324aeae9ff1a7c130952cc07cb06f0
luizpaulo <luizpaulo.siqueira.jr@gmail.com>
2020-10-11 12:03:07 -0300

    feat: cypress test apply to the feature of create and log new user.

--- cypress/integration/examples/new-user.spec.js
+++ cypress/integration/examples/new-user.spec.js
@@ -1,28 +1,19 @@
 const { createYield } = require("typescript");
 
-context("Login Page", () => {
-  it("logs in with valid credentials", () => {
+context("Create a new user", () => {
+  it("create a new user and logs in with valid credentials", () => {
     cy.visit("http://localhost:3000");
 
-    cy.contains("Cadastro");
-    cy.contains("Login");
-    cy.contains("Contato");
-    cy.contains("Quero participar");
-
     cy.get(".sc-lmgQde > :nth-child(3)").click();
-
     cy.get("#basic_company").type("Kenzie");
     cy.get("#basic_email").type("teste@kenzie.com");
     cy.get("#basic_password").type("123456");
     cy.get("#basic_confirmPassword").type("123456");
     cy.get(".ant-btn > .anticon > svg").click();
-
     cy.get('[href="/login"]').click();
-
     cy.get("#email").type("teste@kenzie.com");
     cy.get("#password").type("123456");
     cy.get(".ant-btn-primary").click();
-
     cy.get("#register_email > .ant-input").type("ze@kenzie.com");
     cy.get("#register_password > .ant-input-affix-wrapper > .ant-input").type(
       "123456"
@@ -32,5 +23,6 @@ context("Login Page", () => {
     cy.get("#register_roll > .ant-input").type("analista");
     cy.get("#register_amountLimit > .ant-input").type("400");
     cy.get(".ant-btn > span").click();
+    cy.get('[href="/cadastro"]').click();
   });
 });

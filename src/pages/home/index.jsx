8e68d35356284e6826f34279c36af54d98fb371a
luizpaulo <luizpaulo.siqueira.jr@gmail.com>
2020-10-14 16:27:03 -0300

    feat: add motion at home page.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,6 +11,7 @@ import Kenzie from "../../images/kenzie-logo.png";
 import Google from "../../images/google.png";
 import Philips from "../../images/philips.jpg";
 import Amazon from "../../images/amazon.png";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const { pathname } = useLocation();
@@ -20,56 +21,74 @@ const Home = () => {
   }, [pathname]);
   return (
     <>
-      <Banner
-        bannerTxt1="Otimizando processos de "
-        bannerTxt2="maneira intuitiva e simples"
-        info1="Controle de reembolsos de maneira mais simples."
-        info2="Sistema fácil e intuitivo, sem necessidade de instalação de aplicativos."
-        info3="Venha fazer parte deste jeito simples e direto de reembolsar seus colaboradores."
-      />
-      <Title bckTitle="#365083" colorTitle="#FFFFFF" children="Depoimentos" />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <Banner
+          bannerTxt1="Otimizando processos de "
+          bannerTxt2="maneira intuitiva e simples"
+          info1="Controle de reembolsos de maneira mais simples."
+          info2="Sistema fácil e intuitivo, sem necessidade de instalação de aplicativos."
+          info3="Venha fazer parte deste jeito simples e direto de reembolsar seus colaboradores."
+        />
+        <Title bckTitle="#365083" colorTitle="#FFFFFF" children="Depoimentos" />
 
-      <ClientCard />
-      <Title
-        bckTitle="#FFFFFF"
-        colorTitle="#365083"
-        children="Nossa solução para sua empresa: "
-      />
-      <MainTimeline
-        item1="Controle de reembolso por colaborador."
-        item2="Gestão simplificada de saldo."
-        item3="Timeline com o circuito de aprovação."
-        item4="Comandos intuitivos."
-        item5="Gráficos simples que ajudam na visualização dos valores."
-        item6="Acesso nos principais navegadores da atualidade."
-        item7="Fácil ingresso por smatphone ou tablet."
-        item8="Não precisa instalar aplicativos."
-        item9="Qualquer um pode usar, basta ter conexão com a internet."
-        item10="Disponível 24 horas por dia."
-      />
-      <Title
-        bckTitle="#365083"
-        colorTitle="#FFFFFF"
-        children="Conheça nossos parceiros de sucesso"
-      />
-      <ClientLogo
-        logo1={Kenzie}
-        logo2={Google}
-        logo3={Amazon}
-        logo4={Philips}
-      />
-      <Title
-        bckTitle="#FFFFFF"
-        colorTitle="#365083"
-        children="Contrate nossos serviços"
-      />
-      <HireUs
-        text1="Contato"
-        text2="R. General Mario Tourinho, 1733 - 706, Seminário, Curitiba/PR"
-        text3="0800-000-000"
-        text4="contato@reembolsai.com.br"
-      />
-      <Footer children="Reembolsa.ai Soluções LTDA." />
+        <ClientCard />
+        <Title
+          bckTitle="#FFFFFF"
+          colorTitle="#365083"
+          children="Nossa solução para sua empresa: "
+        />
+        <MainTimeline
+          item1="Controle de reembolso por colaborador."
+          item2="Gestão simplificada de saldo."
+          item3="Timeline com o circuito de aprovação."
+          item4="Comandos intuitivos."
+          item5="Gráficos simples que ajudam na visualização dos valores."
+          item6="Acesso nos principais navegadores da atualidade."
+          item7="Fácil ingresso por smatphone ou tablet."
+          item8="Não precisa instalar aplicativos."
+          item9="Qualquer um pode usar, basta ter conexão com a internet."
+          item10="Disponível 24 horas por dia."
+        />
+        <Title
+          bckTitle="#365083"
+          colorTitle="#FFFFFF"
+          children="Conheça nossos parceiros de sucesso"
+        />
+        <ClientLogo
+          logo1={Kenzie}
+          logo2={Google}
+          logo3={Amazon}
+          logo4={Philips}
+        />
+        <Title
+          bckTitle="#FFFFFF"
+          colorTitle="#365083"
+          children="Contrate nossos serviços"
+        />
+        <HireUs
+          text1="Contato"
+          text2="R. General Mario Tourinho, 1733 - 706, Seminário, Curitiba/PR"
+          text3="0800-000-000"
+          text4="contato@reembolsai.com.br"
+        />
+        <Footer children="Reembolsa.ai Soluções LTDA." />
+      </motion.div>
     </>
   );
 };
